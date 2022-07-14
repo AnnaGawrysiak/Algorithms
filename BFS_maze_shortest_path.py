35dd1a401823da4ff2e71cfa6498795a6727419b
Anna Gawrysiak <70595292+AnnaGawrysiak@users.noreply.github.com>
2022-07-14 13:16:19 +0200

    Add maze_size varaible.

--- BFS_maze_shortest_path.py
+++ BFS_maze_shortest_path.py
@@ -35,9 +35,9 @@ def go_up(node, maze):
         return None
 
 
-def go_down(node, maze):
+def go_down(node, maze, maze_size):
 
-    if node[0] == 10:
+    if node[0] == maze_size:
         return None
 
     possible_going_down = maze[node]['S']
@@ -63,9 +63,9 @@ def go_left(node, maze):
         return None
 
 
-def go_right(node, maze):
+def go_right(node, maze, maze_size):
 
-    if node[1] == 10:
+    if node[1] == maze_size:
         return None
 
     possible_going_right = maze[node]['E']
@@ -77,19 +77,19 @@ def go_right(node, maze):
         return None
 
 
-def get_adjacent(node, maze):
+def get_adjacent(node, maze, maze_size):
 
     adj = []
 
     adj.append(go_up(node, maze))
-    adj.append(go_down(node, maze))
+    adj.append(go_down(node, maze, maze_size))
     adj.append(go_left(node, maze))
-    adj.append(go_right(node, maze))
+    adj.append(go_right(node, maze, maze_size))
 
     return [a for a in adj if a is not None]
 
 
-def bfs(start_node, target_node, maze):
+def bfs(start_node, target_node, maze, maze_size):
 
     parent = dict()
     parent[start_node] = None
@@ -110,7 +110,7 @@ def bfs(start_node, target_node, maze):
             break
 
 
-        adjacent = get_adjacent(node, maze)
+        adjacent = get_adjacent(node, maze,maze_size)
 
         for a in adjacent:
             if a not in visited:
@@ -130,35 +130,23 @@ def bfs(start_node, target_node, maze):
 
 if __name__ == '__main__':
 
-    start_node = (1, 1)
-    end_node = (10, 10)
+    maze_size = 30
 
-    m = maze(10, 10)
+    start_node = (1, 1)
+    end_node = (maze_size, maze_size)
+
+    m = maze(maze_size, maze_size)
     m.CreateMaze(loopPercent=50)
     a = agent(m, filled=True, footprints=True)
 
     maze_map = m.maze_map
     print(m.maze_map)
 
-    #maze_map1 = {(1, 1): {'E': 0, 'W': 0, 'N': 0, 'S': 1}, (2, 1): {'E': 1, 'W': 0, 'N': 1, 'S': 0}, (3, 1): {'E': 1, 'W': 0, 'N': 0, 'S': 1}, (4, 1): {'E': 0, 'W': 0, 'N': 1, 'S': 1}, (5, 1): {'E': 1, 'W': 0, 'N': 1, 'S': 1}, (6, 1): {'E': 1, 'W': 0, 'N': 1, 'S': 0}, (7, 1): {'E': 1, 'W': 0, 'N': 0, 'S': 1}, (8, 1): {'E': 0, 'W': 0, 'N': 1, 'S': 1}, (9, 1): {'E': 0, 'W': 0, 'N': 1, 'S': 1}, (10, 1): {'E': 1, 'W': 0, 'N': 1, 'S': 0},
-                # (1, 2): {'E': 1, 'W': 0, 'N': 0, 'S': 1}, (2, 2): {'E': 0, 'W': 1, 'N': 1, 'S': 1}, (3, 2): {'E': 1, 'W': 1, 'N': 1, 'S': 1}, (4, 2): {'E': 1, 'W': 0, 'N': 1, 'S': 1}, (5, 2): {'E': 0, 'W': 1, 'N': 1, 'S': 0}, (6, 2): {'E': 1, 'W': 1, 'N': 0, 'S': 0}, (7, 2): {'E': 1, 'W': 1, 'N': 0, 'S': 1}, (8, 2): {'E': 0, 'W': 0, 'N': 1, 'S': 1}, (9, 2): {'E': 1, 'W': 0, 'N': 1, 'S': 1}, (10, 2): {'E': 1, 'W': 1, 'N': 1, 'S': 0},
-                # (1, 3): {'E': 1, 'W': 1, 'N': 0, 'S': 1}, (2, 3): {'E': 0, 'W': 0, 'N': 1, 'S': 1}, (3, 3): {'E': 0, 'W': 1, 'N': 1, 'S': 0}, (4, 3): {'E': 1, 'W': 1, 'N': 0, 'S': 1}, (5, 3): {'E': 1, 'W': 0, 'N': 1, 'S': 0}, (6, 3): {'E': 0, 'W': 1, 'N': 0, 'S': 1}, (7, 3): {'E': 1, 'W': 1, 'N': 1, 'S': 0}, (8, 3): {'E': 1, 'W': 0, 'N': 0, 'S': 1}, (9, 3): {'E': 0, 'W': 1, 'N': 1, 'S': 0}, (10, 3): {'E': 1, 'W': 1, 'N': 0, 'S': 0},
-                # (1, 4): {'E': 1, 'W': 1, 'N': 0, 'S': 1}, (2, 4): {'E': 0, 'W': 0, 'N': 1, 'S': 1}, (3, 4): {'E': 0, 'W': 0, 'N': 1, 'S': 1}, (4, 4): {'E': 1, 'W': 1, 'N': 1, 'S': 0}, (5, 4): {'E': 0, 'W': 1, 'N': 0, 'S': 1}, (6, 4): {'E': 1, 'W': 0, 'N': 1, 'S': 1}, (7, 4): {'E': 0, 'W': 1, 'N': 1, 'S': 1}, (8, 4): {'E': 0, 'W': 1, 'N': 1, 'S': 0}, (9, 4): {'E': 1, 'W': 0, 'N': 0, 'S': 1}, (10, 4): {'E': 0, 'W': 1, 'N': 1, 'S': 0},
-                # (1, 5): {'E': 1, 'W': 1, 'N': 0, 'S': 1}, (2, 5): {'E': 0, 'W': 0, 'N': 1, 'S': 1}, (3, 5): {'E': 0, 'W': 0, 'N': 1, 'S': 1}, (4, 5): {'E': 1, 'W': 1, 'N': 1, 'S': 0}, (5, 5): {'E': 1, 'W': 0, 'N': 0, 'S': 1}, (6, 5): {'E': 0, 'W': 1, 'N': 1, 'S': 1}, (7, 5): {'E': 1, 'W': 0, 'N': 1, 'S': 0}, (8, 5): {'E': 1, 'W': 0, 'N': 0, 'S': 1}, (9, 5): {'E': 0, 'W': 1, 'N': 1, 'S': 0}, (10, 5): {'E': 1, 'W': 0, 'N': 0, 'S': 0},
-                # (1, 6): {'E': 1, 'W': 1, 'N': 0, 'S': 1}, (2, 6): {'E': 1, 'W': 0, 'N': 1, 'S': 0}, (3, 6): {'E': 1, 'W': 0, 'N': 0, 'S': 0}, (4, 6): {'E': 0, 'W': 1, 'N': 0, 'S': 1}, (5, 6): {'E': 1, 'W': 1, 'N': 1, 'S': 1}, (6, 6): {'E': 1, 'W': 0, 'N': 1, 'S': 1}, (7, 6): {'E': 1, 'W': 1, 'N': 1, 'S': 1}, (8, 6): {'E': 0, 'W': 1, 'N': 1, 'S': 1}, (9, 6): {'E': 0, 'W': 0, 'N': 1, 'S': 1}, (10, 6): {'E': 1, 'W': 1, 'N': 1, 'S': 0},
-                # (1, 7): {'E': 1, 'W': 1, 'N': 0, 'S': 0}, (2, 7): {'E': 1, 'W': 1, 'N': 0, 'S': 0}, (3, 7): {'E': 0, 'W': 1, 'N': 0, 'S': 1}, (4, 7): {'E': 0, 'W': 0, 'N': 1, 'S': 1}, (5, 7): {'E': 1, 'W': 1, 'N': 1, 'S': 0}, (6, 7): {'E': 1, 'W': 1, 'N': 0, 'S': 0}, (7, 7): {'E': 1, 'W': 1, 'N': 0, 'S': 1}, (8, 7): {'E': 1, 'W': 0, 'N': 1, 'S': 0}, (9, 7): {'E': 1, 'W': 0, 'N': 0, 'S': 1}, (10, 7): {'E': 0, 'W': 1, 'N': 1, 'S': 0},
-                # (1, 8): {'E': 1, 'W': 1, 'N': 0, 'S': 1}, (2, 8): {'E': 0, 'W': 1, 'N': 1, 'S': 1}, (3, 8): {'E': 0, 'W': 0, 'N': 1, 'S': 1}, (4, 8): {'E': 1, 'W': 0, 'N': 1, 'S': 1}, (5, 8): {'E': 1, 'W': 1, 'N': 1, 'S': 1}, (6, 8): {'E': 0, 'W': 1, 'N': 1, 'S': 0}, (7, 8): {'E': 1, 'W': 1, 'N': 0, 'S': 0}, (8, 8): {'E': 1, 'W': 1, 'N': 0, 'S': 0}, (9, 8): {'E': 0, 'W': 1, 'N': 0, 'S': 1}, (10, 8): {'E': 1, 'W': 0, 'N': 1, 'S': 0},
-                # (1, 9): {'E': 1, 'W': 1, 'N': 0, 'S': 0}, (2, 9): {'E': 1, 'W': 0, 'N': 0, 'S': 0}, (3, 9): {'E': 1, 'W': 0, 'N': 0, 'S': 1}, (4, 9): {'E': 0, 'W': 1, 'N': 1, 'S': 0}, (5, 9): {'E': 0, 'W': 1, 'N': 0, 'S': 1}, (6, 9): {'E': 0, 'W': 0, 'N': 1, 'S': 1}, (7, 9): {'E': 1, 'W': 1, 'N': 1, 'S': 0}, (8, 9): {'E': 1, 'W': 1, 'N': 0, 'S': 1}, (9, 9): {'E': 1, 'W': 0, 'N': 1, 'S': 0}, (10, 9): {'E': 1, 'W': 1, 'N': 0, 'S': 0},
-                # (1, 10): {'E': 0, 'W': 1, 'N': 0, 'S': 1}, (2, 10): {'E': 0, 'W': 1, 'N': 1, 'S': 0}, (3, 10): {'E': 0, 'W': 1, 'N': 0, 'S': 1}, (4, 10): {'E': 0, 'W': 0, 'N': 1, 'S': 1}, (5, 10): {'E': 0, 'W': 0, 'N': 1, 'S': 1}, (6, 10): {'E': 0, 'W': 0, 'N': 1, 'S': 1}, (7, 10): {'E': 0, 'W': 1, 'N': 1, 'S': 1}, (8, 10): {'E': 0, 'W': 1, 'N': 1, 'S': 0}, (9, 10): {'E': 0, 'W': 1, 'N': 0, 'S': 1}, (10, 10): {'E': 0, 'W': 1, 'N': 1, 'S': 0}}
-
-
-    path = bfs(start_node, end_node, maze_map)
+    path = bfs(start_node, end_node, maze_map, maze_size)
     print(path)
 
     for coordinates in path:
         a.position = coordinates
-
-    #a.position = (10, 9)
 
     m.run()
 
